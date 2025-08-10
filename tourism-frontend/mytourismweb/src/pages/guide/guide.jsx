6d2eec146aad0551df05b0010567a20604f406d5
Umair Waheed <122041430+Umair-Waheed@users.noreply.github.com>
2025-08-10 15:33:33 +0500

    Update guide.jsx

--- tourism-frontend/mytourismweb/src/pages/guide/guide.jsx
+++ tourism-frontend/mytourismweb/src/pages/guide/guide.jsx
@@ -3,7 +3,6 @@ import "./guide.css";
 import Searchbar from "../../components/Searchbar/Searchbar";
 import Card from "../../components/Card/Card.jsx";
 import { StoreContext } from "../../context/StoreContext.jsx";
-import CategoryCircle from "../../components/CategoryCircle/categoryCircle.jsx";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import axios from "axios";
 import { assets } from "../../assets/assets.js";
@@ -71,17 +70,7 @@ const guide = () => {
           />
         </div>
       </div>
-      {/* <div className="hotel-category-container">
-          <div className="hotel-category">
-            <CategoryCircle name={"Mountains"} img={assets.mountain_cat}  onClick={categoryHandler}  category={category}/>
-            <CategoryCircle name={"Nature & Wildlife"} img={assets.Wildlife_cat}  onClick={categoryHandler}  category={category}/>
-            <CategoryCircle name={"Adventure Spots"} img={assets.adventure_cat}  onClick={categoryHandler}  category={category}/>
-            <CategoryCircle name={"Historical guides"} img={assets.historical_cat}  onClick={categoryHandler}  category={category}/>
-            <CategoryCircle name={"Hiking"} img={assets.hiking_cat}  onClick={categoryHandler}  category={category}/>
-            <CategoryCircle name={"Water Spots"} img={assets.waterspot_cat}  onClick={categoryHandler}  category={category}/>
-            <CategoryCircle name={"Camping"} img={assets.camping_cat}  onClick={categoryHandler}  category={category}/>
-          </div>
-        </div> */}
+     
       <div className="guide-container px-20">
         <div className="guide-content-container">
           <h2>Guides</h2>
