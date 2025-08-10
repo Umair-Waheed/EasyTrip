e14a237a178f38b4129f63accd39d87a0dde1af6
Umair Waheed <122041430+Umair-Waheed@users.noreply.github.com>
2025-08-10 13:47:13 +0500

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB=async()=>{
-        await mongoose.connect(process.env.MONGODB_URL)
-        .then(()=>console.log("DataBase is Connected"))
-}
